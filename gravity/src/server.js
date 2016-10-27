8e8da0ed766b1c53e8990a98114bdfc779057006
Matt Drapchaty <Drapchaty@comcast.net>
2016-10-27 16:05:32 -0600

    help

--- gravity/src/server.js
+++ gravity/src/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+	res.json({healthy: true})
+});
+
 app.listen(3000, function(){
 	console.log("Server is running on Port 3000.");
 });
